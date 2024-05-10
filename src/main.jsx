9fd4f1abafcebb0bf170f97741dc1ceac7c721db
Anamul Karim <anamulkarim4567@gmail.com>
2024-05-10 17:50:46 +0600

    added login

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/Routes.jsx'
+// import router from './routes/Routes.jsx'
+// import { Root } from 'postcss'
+// import Home from './pages/Home/Home.jsx'
+// import Login from './pages/Login/Login.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// const router = createBrowserRouter([
+  
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//       <RouterProvider router={router} />
+//   </React.StrictMode>,
+// )
+
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./layouts/Root";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path:"/login",
+        element:<Login></Login>
+      }
+
+    ]
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
